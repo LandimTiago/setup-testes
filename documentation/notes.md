3521ec2c88a2ca527b3cbe7bfcc3bd62b85c3ed4
Tiago landim <tiago.landim.ext@brf.com>
2024-03-14 13:46:06 -0300

    ensure CheckLastEventStatus calls LoadLastEvent with correct groupId

---
+++ documentation/notes.md
@@ -0,0 +1,21 @@
+# Anti-Patterns/Code Smells
+
+- Speculative Generality: especular coisas que poderá precisar sem antes precisar
+- God Class: Classes que fazer tudo
+- Divergent Change: Alterar o arquivo por mais de um motivo
+- Improper Instantiation: Criar instancias de forma errada
+- High Coupling
+
+# Design Patterns/Principles/Conventions
+
+- You ain't Gonna Need It (YAGNI)
+- Single Responsibility (SRP)
+- Liskov Substitution (LSP)
+- Dependence Inversion (DIP)
+- Arrange, Act, Assert (AAA)
+
+- Dependence injection (DI)
+- Repository Pattern
+- Test Doubles
+
+- Small Commits
---
+++ test/check-last-event-status.spec.ts
@@ -0,0 +1,37 @@
+// Use Case
+class CheckEventLastStatus {
+  constructor(
+    private readonly loadLastEventRepository: LoadLastEventRepository
+  ) {}
+
+  async perform(groupId: string): Promise<void> {
+    this.loadLastEventRepository.loadLastEvent("any_group_id");
+  }
+}
+
+// interface é um contrato
+interface LoadLastEventRepository {
+  loadLastEvent: (groupId: string) => Promise<void>;
+}
+
+class LoadLastEventRepositoryMock implements LoadLastEventRepository {
+  groupId?: string;
+
+  async loadLastEvent(groupId: string): Promise<void> {
+    this.groupId = groupId;
+  }
+}
+
+describe("CheckEventLastStatus", () => {
+  it(" Should get last event data ", async () => {
+    const loadLastEventRepository = new LoadLastEventRepositoryMock();
+    const checkLastEventStatus = new CheckEventLastStatus(
+      loadLastEventRepository
+    );
+
+    await checkLastEventStatus.perform("any_group_id");
+
+    // Estou garantindo que o ID que será chamado é exatamente o que foi passado para o repository
+    expect(loadLastEventRepository.groupId).toBe("any_group_id");
+  });
+});
